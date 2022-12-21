ef43d35bf1eaa9d4dde2454d79e680952d69eb41
arnold cubici-jones <arnoldcubicijones@hotmail.co.uk>
2022-12-21 14:54:39 +0000

    navbar dissapears scroll down, reappears scroll up

---
+++ app/javascript/controllers/navbar_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+let scroll_position = 0;
+let scroll_direction;
+// Connects to data-controller="navbar"
+export default class extends Controller {
+  connect() {
+    console.log("connects");
+  }
+
+  update() {
+    if (window.scrollY >= window.innerHeight - (window.innerHeight * 0.1)) {
+      this.element.classList.add("contraster")
+    } else {
+      this.element.classList.remove("contraster")
+    }
+
+    scroll_direction = (document.body.getBoundingClientRect()).top > scroll_position ? 'up' : 'down';
+    scroll_position = (document.body.getBoundingClientRect()).top;
+    console.log(scroll_direction);
+
+    if (scroll_direction == 'down') {
+      this.element.classList.add("hider")
+    } else {
+      this.element.classList.remove("hider")
+    };
+
+  }
+}
