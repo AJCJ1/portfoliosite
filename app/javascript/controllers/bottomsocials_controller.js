8e7130bfff59afaa4a9e00c7c567218d09a0f565
arnold cubici-jones <arnoldcubicijones@hotmail.co.uk>
2022-12-23 10:52:29 +0000

    bottom socials dissapear at top

--- app/javascript/controllers/bottomsocials_controller.js
+++ app/javascript/controllers/bottomsocials_controller.js
@@ -1,16 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="socials"
+// Connects to data-controller="bottomsocials"
 export default class extends Controller {
-  static targets = ["social"]
+  static targets = ["bottomsocials"]
+
   connect() {
   }
 
   update() {
     if (window.scrollY >= window.innerHeight / 3) {
-      this.element.classList.add("socials-back")
+      this.element.classList.add("socials-b-show")
     } else {
-      this.element.classList.remove("socials-back")
+      this.element.classList.remove("socials-b-show")
     }
   }
 }
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import BottomsocialsController from "./bottomsocials_controller"
+application.register("bottomsocials", BottomsocialsController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
