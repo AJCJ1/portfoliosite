94ff9acf73baf1f0c0fe938e11f90a063be25b65
arnold cubici-jones <arnoldcubicijones@hotmail.co.uk>
2023-01-04 10:10:50 +0000

    adds qr code to see on mob

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
+    @qr_code = RQRCode::QRCode.new("https://www.arnoldcjones.co.uk")
+    @qr_as_svg = @qr_code.as_svg(
+      offset: 0,
+      shape_rendering: 'crispEdges',
+      standalone: true,
+      color: "96C3F6",
+      viewbox: true
+    )
   end
 end
